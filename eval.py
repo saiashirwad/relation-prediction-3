61dea88a0562cae301566c826d1b087eb450613b
Sai Ashirwad <sai@pop-os.localdomain>
2020-05-04 10:58:31 +0530

    eh

--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-import torch 
+import torch
 from torch.utils.data import DataLoader
 
 from utils import negative_sampling
@@ -9,24 +9,24 @@ def generate_eval_triplets(triplet, pos="head", n_ent=14541):
         triplets = triplet.repeat(n_ent).view(-1, 2).t()
         triplets = torch.stack((
             torch.arange(n_ent),
-            triplets[0], 
+            triplets[0],
             triplets[1]
         ))
     elif pos == "tail":
         triplet = torch.tensor([triplet[0].item(), triplet[2].item()]).to(torch.long)
         triplets = triplet.repeat(n_ent).view(-1, 2).t()
         triplets = torch.stack((
-            triplets[0], 
+            triplets[0],
             torch.arange(n_ent),
             triplets[1]
         ))
-    
+
     return triplets.t()
 
 def eval(kg_val, model, n_dim, n_samples):
     dataloader = DataLoader(kg_val, 1, shuffle=True)
     data = [d for d in dataloader]
-    
+
     n = n_samples
 
     n_ent = kg_val.n_ent
@@ -38,10 +38,10 @@ def eval(kg_val, model, n_dim, n_samples):
     with torch.no_grad():
         for i in range(n_samples):
             triplets_h = generate_eval_triplets(data[i], "head", n_ent)
-            triplets_h, _, _, _ = negative_sampling(triplets_h, n_ent, 0)
+            triplets_h, _ = negative_sampling(triplets_h, n_ent, 0)
             triplets_h = triplets_h.to("cuda")
             ee, re = model(triplets_h)
-        
+
             dst = ee[data[i][1]].squeeze()
             rel = re[data[i][2]].squeeze()
             dist = ee + (rel - dst).repeat(n_ent).view(-1, 100)
@@ -49,14 +49,14 @@ def eval(kg_val, model, n_dim, n_samples):
             rank = head_preds.index(data[i][0])
             head_rank_mean += rank
             if rank < 10:
-                head_hits_10 += 1 
-        
+                head_hits_10 += 1
+
             # # # # tail
             triplets_t = generate_eval_triplets(data[i], "tail", n_ent)
             triplets_t, _, _, _ = negative_sampling(triplets_t, n_ent, 0)
             triplets_t = triplets_t.to("cuda")
             ee, re = model(triplets_t)
-        
+
             src = ee[data[i][0]].squeeze()
             rel = re[data[i][2]].squeeze()
             dist = (src + rel).repeat(n_ent).view(-1, 100) - ee
@@ -66,8 +66,8 @@ def eval(kg_val, model, n_dim, n_samples):
             if rank < 10:
                 tail_hits_10 += 1
 
-        head_rank_mean /= n 
-        tail_rank_mean /= n 
+        head_rank_mean /= n
+        tail_rank_mean /= n
         head_hits_10 /= n
         tail_hits_10 /= n
         mean_rank = (head_rank_mean + tail_rank_mean) / 2
